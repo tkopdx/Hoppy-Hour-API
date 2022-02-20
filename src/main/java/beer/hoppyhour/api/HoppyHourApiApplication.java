66d20b502a118081fbc77d5667fc926818fdff07
tkopdx <63491466+tkopdx@users.noreply.github.com>
2022-02-21 06:13:16 +0900

    first commit

---
+++ src/main/java/beer/hoppyhour/api/HoppyHourApiApplication.java
@@ -0,0 +1,13 @@
+package beer.hoppyhour.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoppyHourApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoppyHourApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/beer/hoppyhour/api/HoppyHourApiApplicationTests.java
@@ -0,0 +1,13 @@
+package beer.hoppyhour.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoppyHourApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
