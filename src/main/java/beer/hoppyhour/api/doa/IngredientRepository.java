8df832dfd4a4e1903018102affcf9af69ce80bec
tkopdx <63491466+tkopdx@users.noreply.github.com>
2022-04-12 14:24:15 +0900

    added more specific parameters to ingredient repo and ingredient detail repo

--- src/main/java/beer/hoppyhour/api/doa/IngredientRepository.java
+++ src/main/java/beer/hoppyhour/api/doa/IngredientRepository.java
@@ -3,9 +3,10 @@ package beer.hoppyhour.api.doa;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import beer.hoppyhour.api.entity.Ingredient;
+import beer.hoppyhour.api.entity.IngredientDetail;
 
 //TODO Creating a repository from a raw type may be poor practice.
 //See IngredientDetailRepository for similar issue and rationale.
-public interface IngredientRepository extends PagingAndSortingRepository<Ingredient<?>, Long> {
+public interface IngredientRepository extends PagingAndSortingRepository<Ingredient<? extends IngredientDetail<?>>, Long> {
 
 }
